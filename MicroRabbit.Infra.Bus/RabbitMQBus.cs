334e593045e678a0c330fa0cfa9fcc0a0e6f20e0
Victor Obaitor <Victor.Obaitor@unitedcapitalplcgroup.com>
2021-06-29 17:21:12 +0100

    implemented publish and subscribe method

--- MicroRabbit.Infra.Bus/RabbitMQBus.cs
+++ MicroRabbit.Infra.Bus/RabbitMQBus.cs
@@ -7,6 +7,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RabbitMQ.Client;
+using Newtonsoft.Json;
 
 namespace MicroRabbit.Infra.Bus
 {
@@ -28,13 +30,41 @@ namespace MicroRabbit.Infra.Bus
         }
        public void Publish<T>(T @event) where T : Event
         {
-            throw new NotImplementedException();
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                var eventName = @event.GetType().Name;
+                channel.QueueDeclare(eventName, false, false, false, null);
+
+                var message = JsonConvert.SerializeObject(@event);
+                var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish("", eventName, null, body);
+            }
         }
         public void Subscribe<T, TH>()
             where T : Event
             where TH : IEventHandler<T>
         {
-            throw new NotImplementedException();
+            var eventName = typeof(T).Name;
+            var handlerType = typeof(TH);
+            if (!_eventTypes.Contains(typeof(T)))
+            {
+                _eventTypes.Add(typeof(T));
+            }
+            if (!_handlers.ContainsKey(eventName))
+            {
+                _handlers.Add(eventName, new List<Type>());
+            }
+            if (_handlers[eventName].Any(s => s.GetType() == handlerType))
+            {
+                throw new ArgumentException($"Handler Type {handlerType.Name} already is registered for '{eventName}'");
+            }
+            _handlers[eventName].Add(handlerType);
         }
+
+
+
+
     }
 }
