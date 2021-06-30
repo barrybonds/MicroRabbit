6bbd8cc6169162e8f198e4b38f6597acac083ec6
Victor Obaitor <Victor.Obaitor@unitedcapitalplcgroup.com>
2021-06-30 02:22:36 +0100

    Added DBContext, DbSet and Repository code.

---
+++ MicroRabbit.Banking.Domain/Models/Account.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Banking.Domain.Models
+{
+   public class Account
+    {
+        public int Id { get; set; }
+        public string AccoutType { get; set; }
+        public decimal Accountbalance { get; set; }
+    }
+}
