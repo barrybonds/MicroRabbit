45458883e36266db7d0af4a0f04c34e693f190ae
Victor Obaitor <Victor.Obaitor@unitedcapitalplcgroup.com>
2021-07-11 03:55:40 +0100

    Added Tranfer API controller and

---
+++ MicroRabbit.Transfer.Api/Controllers/TransferLogsController.cs
@@ -0,0 +1,42 @@
+﻿using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Application.Models;
+using MicroRabbit.Banking.Domain.Models;
+using MicroRabbit.Transfer.Application.Interfaces;
+using MicroRabbit.Transfer.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Banking.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransferLogsController : ControllerBase
+    {
+        private readonly ITransferService _transferService;
+
+        public TransferLogsController(ITransferService transferService)
+        {
+            _transferService = transferService;
+        }
+
+        //GET api/banking
+        [HttpGet]
+        public ActionResult<IEnumerable<TransferLog>> GetTransfer()
+        {
+            return Ok(_transferService.GetTransferLogs());
+        }
+
+        //[HttpPost]
+        //public IActionResult Post([FromBody] AccountTransfer accountTransfer)
+        //{
+        //    _accountService.Transfer(accountTransfer);
+
+        //    return Ok(accountTransfer);
+        //}
+
+
+    }
+}
---
+++ MicroRabbit.Transfer.Data/Repository/TransferRepository.cs
@@ -0,0 +1,29 @@
+﻿using MicroRabbit.Transfer.Data.Context;
+using MicroRabbit.Transfer.Domain.Interfaces;
+using MicroRabbit.Transfer.Domain.Models;
+using MicroRabbit.Transfer.Data.Context;
+using System; 
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Transfer.Data.Repository
+{
+    public class TransferRepository : ITransferRepository
+    {
+        private TransferDbContext _ctx;
+
+        public TransferRepository(TransferDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public IEnumerable<TransferLog> GetTransferLogs()
+        {
+            return _ctx.Accounts;
+        }
+
+
+    }
+}
